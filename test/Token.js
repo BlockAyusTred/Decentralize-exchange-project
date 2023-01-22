c7ad1cff2afe2559c6326c6863ec1aadac9617fd
Ayush anand <forcasualuse212@gmaill.com>
2023-01-22 06:41:09 +0530

    added balance function in smart contract and test

--- test/Token.js
+++ test/Token.js
@@ -6,11 +6,16 @@ const tokens = (n) => {
 
 describe("Token", function () {
   let token;
+  let accounts
+  let deployer;
 
   beforeEach(async () => {
     // Fetch token contract from blockchain
     const Token = await ethers.getContractFactory("Token");
     token = await Token.deploy("Ayush Anand", "AAND", 1000000);
+
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
   });
 
   describe("Deployment", () => {
@@ -28,6 +33,10 @@ describe("Token", function () {
 
     it("has correct totalSupply", async () => {
       expect(await token.totalSupply()).to.equal(tokens(1000000));
+    });
+
+    it("assign total supply to deployer", async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(tokens(1000000));
     });
   });
 });
