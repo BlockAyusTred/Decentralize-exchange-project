bcc218fa27390c3873eca3e87c0a761774770f02
Ayush anand <forcasualuse212@gmaill.com>
2023-02-03 08:47:45 +0530

    My orders

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ import Markets from "./Markets";
 import Balance from "./Balance";
 import Order from "./Order";
 import PriceChart from "./PriceChart";
+import Transactions from "./Transactions";
 import Trades from "./Trades";
 import OrderBook from "./OrderBook";
 
@@ -77,7 +78,7 @@ function App() {
         <section className="exchange__section--right grid">
           <PriceChart />
 
-          {/* Transactions */}
+          <Transactions />
 
           <Trades />
 
